26f0a9ce1306a85135ce39bac4fa19a25d964d39
Thomas Czogalik <thomas.czogalik@andrena.de>
2021-05-12 15:49:46 +0200

    fix

--- elevator.js
+++ elevator.js
@@ -2,10 +2,14 @@
     init: function init(elevators, floors) {
         console.clear();
 
-        const debug = 'Hello'
-        console.log(debug)
-
         const e = elevators[0]; // Let's use the first elevator
+
+        floors.forEach(function (floor) {
+            // floor button pressed on floor
+            floor.on("up_button_pressed down_button_pressed", function () {
+                e.goToFloor(floor.floorNum());
+            });
+        });
 
         // event if elevator is doing nothing...
         e.on("idle", idle);
@@ -20,24 +24,28 @@
                 this.e = e;
             }
             run(floorNum) {
-                this.e.goToFloor(floorNum);
+                e.goToFloor(floorNum);
             }
         }
         var stateHandler = new FloorButtonPressedStateHandler(e)
         e.on("floor_button_pressed", stateHandler.run);
 
+
         e.on("passing_floor", function (floorNum) {
-            // do something
+            let p = e.getPressedFloors();
+            // if we're going in the same direction as the button, we can stop
+            if (p.indexOf(floorNum) >= 0) {
+                // remove this floor from destinations
+                e.destinationQueue = e.destinationQueue.filter((d) => (d !== floorNum));
+                e.checkDestinationQueue();
+
+                e.goToFloor(floorNum, true);
+            }
+
         });
 
-        floors.forEach(function (floor) {
-            // floor button pressed on floor
-            floor.on("up_button_pressed down_button_pressed", function () {
-                e.goToFloor(floor.floorNum());
-            });
-        });
     },
-    update: function update(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+        update: function update(dt, elevators, floors) {
+            // We normally don't need to do anything here
+        }
 }
